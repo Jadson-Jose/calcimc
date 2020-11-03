c27996dd6777e4c3d8fb4386981773b80222d831
JadsonJose <jadsonjosesilva@gmail.com>
2020-11-03 20:36:59 -0300

    Initial commit

---
+++ calcimc/App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default function calcimc() {
+  return (
+    <SafeAreaView>
+      <Text>Calculadora de IMC</Text>
+    </SafeAreaView>
+  )
+}
+
+
+const estilos = StyleSheet.create({
+
+
+});
+
+

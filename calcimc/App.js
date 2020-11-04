cb22dc0005665766a551a1593d71bf1af59f4153
JadsonJose <jadsonjosesilva@gmail.com>
2020-11-04 17:38:05 -0300

    sinc

--- calcimc/App.js
+++ calcimc/App.js
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,19 +6,65 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableHighlight,
+  TextInput
 } from 'react-native';
 
 export default function calcimc() {
+
+  const [peso, setPeso] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
   return (
-    <SafeAreaView>
-      <Text>Calculadora de IMC</Text>
+    <SafeAreaView style={estilos.corpo}>
+      <View style={estilos.bloco}>
+        <Text>Calculadora de IMC</Text>
+      </View>
+      <View>
+        <Text>Informe seu peso</Text>
+        <TextInput
+          style={estilos.txt}
+          autoFocus={true}
+          keyboardType={"numeric"}
+          onChangeText={text => setPeso(text)}
+        ></TextInput>
+      </View>
+      <View>
+        <Text>Informe a sua altura</Text>
+        <TextInput
+          style={estilos.txt}
+          autoFocus={false}
+          keyboardType={"numeric"}
+          onChangeText={text => setAltura(text)}
+        ></TextInput>
+      </View>
+      <View>
+        <TouchableHighlight
+
+        >
+          <Text>Calcular IMC</Text>
+        </TouchableHighlight>
+      </View>
     </SafeAreaView>
   )
 }
 
 
 const estilos = StyleSheet.create({
-
+  corpo: {
+    padding: 10
+  },
+  bloco: {
+    marginBottom: 20
+  },
+  txt: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 10,
+    borderRadius: 10,
+  }
 
 });
 
